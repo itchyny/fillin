56cc358c52f983b7337a53c64aa093e73985930a
itchyny <itchyny@hatena.ne.jp>
2017-06-09 01:12:27 +0900

    ask value for each identifier once

--- resolve.go
+++ resolve.go
@@ -11,9 +11,11 @@ func Resolve(identifiers []string) map[string]string {
 	reader := bufio.NewReader(os.Stdin)
 	values := make(map[string]string, len(identifiers))
 	for _, identifier := range identifiers {
-		fmt.Printf("%s: ", identifier)
-		text, _ := reader.ReadString('\n')
-		values[identifier] = strings.TrimSuffix(text, "\n")
+		if _, ok := values[identifier]; !ok {
+			fmt.Printf("%s: ", identifier)
+			text, _ := reader.ReadString('\n')
+			values[identifier] = strings.TrimSuffix(text, "\n")
+		}
 	}
 	return values
 }
