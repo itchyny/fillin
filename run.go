d225c39b3310cbddb55d1d1e5c3fae6ce461708b
itchyny <itchyny@hatena.ne.jp>
2017-06-08 23:43:44 +0900

    use cmd /c on Windows

--- run.go
+++ run.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"os/exec"
+	"runtime"
 	"syscall"
 
 	"github.com/kballard/go-shellquote"
 )
 
+var cmdBase = []string{"sh", "-c"}
+
+func init() {
+	if runtime.GOOS == "windows" {
+		cmdBase = []string{"cmd", "/c"}
+	}
+}
+
 func run(args []string, env []string) error {
-	sh, err := exec.LookPath("sh")
+	sh, err := exec.LookPath(cmdBase[0])
 	if err != nil {
 		return err
 	}
-	cmd := []string{"sh", "-c", shellquote.Join(args...)}
+	cmd := append(cmdBase, shellquote.Join(args...))
 	if err := syscall.Exec(sh, cmd, env); err != nil {
 		return err
 	}
