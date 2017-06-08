8ca38b7cef244f32285bba5600e8caa38f6ecdff
itchyny <itchyny@hatena.ne.jp>
2017-06-09 01:12:04 +0900

    make build directory have priority over any other paths in $PATH

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ import (
 
 func TestMain(t *testing.T) {
 	build, _ := filepath.Abs("build")
-	envPath := os.Getenv("PATH") + ":" + build
+	envPath := build + ":" + os.Getenv("PATH")
 	filepath.Walk("test", func(path string, info os.FileInfo, err error) error {
 		if strings.HasSuffix(path, ".sh") {
 			cmd := exec.Command("bash", filepath.Base(path))
