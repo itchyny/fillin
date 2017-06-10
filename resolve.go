215b4703785173c537eeef6a0b62c9d81aad6050
itchyny <itchyny@hatena.ne.jp>
2017-06-11 05:59:23 +0900

    quick continue to reduce indent depth

--- resolve.go
+++ resolve.go
@@ -21,76 +21,79 @@ func Resolve(identifiers []*Identifier, config *Config, in *bufio.Reader, out *b
 	scopeAsked := make(map[string]bool)
 	if in == nil {
 		for _, id := range identifiers {
-			if !found(values, id) && id.scope != "" && !scopeAsked[id.scope] {
-				scopeAsked[id.scope] = true
-				var keys []string
-				added := make(map[string]bool)
-				for _, d := range identifiers {
-					if id.scope == d.scope && !added[d.key] {
-						keys = append(keys, d.key)
-						added[d.key] = true
-					}
+			if found(values, id) || id.scope == "" || scopeAsked[id.scope] {
+				continue
+			}
+			scopeAsked[id.scope] = true
+			var keys []string
+			added := make(map[string]bool)
+			for _, d := range identifiers {
+				if id.scope == d.scope && !added[d.key] {
+					keys = append(keys, d.key)
+					added[d.key] = true
 				}
-				if len(keys) > 0 {
-					hs := config.historyPairs(&IdentifierGroup{scope: id.scope, keys: keys})
-					if len(hs) == 0 {
-						continue
-					}
-					line.ClearHistory()
-					for i := len(hs) - 1; i >= 0; i-- {
-						line.AppendHistory(hs[i])
-					}
-					prompt := fmt.Sprintf("[%s] %s: ", id.scope, strings.Join(keys, ", "))
-					text, err := line.Prompt(prompt)
-					if err != nil {
-						if err == liner.ErrPromptAborted || err == io.EOF {
-							os.Exit(1)
-						}
-						log.Fatal(err)
-					}
-					xs := strings.Split(strings.TrimSuffix(text, "\n"), ", ")
-					if len(xs) == len(keys) {
-						for i, key := range keys {
-							id := &Identifier{scope: id.scope, key: key}
-							insert(values, id, strings.Replace(xs[i], ",\\ ", ", ", -1))
-						}
-					}
+			}
+			if len(keys) == 0 {
+				continue
+			}
+			hs := config.historyPairs(&IdentifierGroup{scope: id.scope, keys: keys})
+			if len(hs) == 0 {
+				continue
+			}
+			line.ClearHistory()
+			for i := len(hs) - 1; i >= 0; i-- {
+				line.AppendHistory(hs[i])
+			}
+			prompt := fmt.Sprintf("[%s] %s: ", id.scope, strings.Join(keys, ", "))
+			text, err := line.Prompt(prompt)
+			if err != nil {
+				if err == liner.ErrPromptAborted || err == io.EOF {
+					os.Exit(1)
+				}
+				log.Fatal(err)
+			}
+			xs := strings.Split(strings.TrimSuffix(text, "\n"), ", ")
+			if len(xs) == len(keys) {
+				for i, key := range keys {
+					id := &Identifier{scope: id.scope, key: key}
+					insert(values, id, strings.Replace(xs[i], ",\\ ", ", ", -1))
 				}
 			}
 		}
 	}
 
 	for _, id := range identifiers {
-		if !found(values, id) {
-			prompt := fmt.Sprintf("%s: ", id.key)
-			if id.scope != "" {
-				prompt = fmt.Sprintf("[%s] %s: ", id.scope, id.key)
-			}
-			var text string
-			var err error
-			if in == nil {
-				line.ClearHistory()
-				hs := config.history(id)
-				for i := len(hs) - 1; i >= 0; i-- {
-					line.AppendHistory(hs[i])
-				}
-				text, err = line.Prompt(prompt)
-				if err != nil {
-					if err == liner.ErrPromptAborted || err == io.EOF {
-						os.Exit(1)
-					}
-					log.Fatal(err)
-				}
-			} else {
-				out.WriteString(prompt)
-				out.Flush()
-				text, err = in.ReadString('\n')
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			insert(values, id, strings.TrimSuffix(text, "\n"))
+		if found(values, id) {
+			continue
 		}
+		prompt := fmt.Sprintf("%s: ", id.key)
+		if id.scope != "" {
+			prompt = fmt.Sprintf("[%s] %s: ", id.scope, id.key)
+		}
+		var text string
+		var err error
+		if in == nil {
+			line.ClearHistory()
+			hs := config.history(id)
+			for i := len(hs) - 1; i >= 0; i-- {
+				line.AppendHistory(hs[i])
+			}
+			text, err = line.Prompt(prompt)
+			if err != nil {
+				if err == liner.ErrPromptAborted || err == io.EOF {
+					os.Exit(1)
+				}
+				log.Fatal(err)
+			}
+		} else {
+			out.WriteString(prompt)
+			out.Flush()
+			text, err = in.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		insert(values, id, strings.TrimSuffix(text, "\n"))
 	}
 
 	return values
