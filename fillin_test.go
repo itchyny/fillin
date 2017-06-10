d62251de02e17a3a1d1ee492549bc43af325b31d
itchyny <itchyny@hatena.ne.jp>
2017-06-10 21:55:28 +0900

    add tests for collectIdentifiers

---
+++ fillin_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var collectIdentifiersTests = []struct {
+	args []string
+	ids  []*Identifier
+}{
+	{
+		args: []string{"{{foo}}", "{{bar}}", "{{baz}}"},
+		ids: []*Identifier{
+			&Identifier{key: "foo"},
+			&Identifier{key: "bar"},
+			&Identifier{key: "baz"},
+		},
+	},
+	{
+		args: []string{"{{ foo }}", "{{\tbar\t}}", "{{   baz  \t \t }}"},
+		ids: []*Identifier{
+			&Identifier{key: "foo"},
+			&Identifier{key: "bar"},
+			&Identifier{key: "baz"},
+		},
+	},
+	{
+		args: []string{"{{foo:bar}}", "{{  foo : baz  }}", "{{foo}}"},
+		ids: []*Identifier{
+			&Identifier{scope: "foo", key: "bar"},
+			&Identifier{scope: "foo", key: "baz"},
+			&Identifier{scope: "", key: "foo"},
+		},
+	},
+}
+
+func Test_collectIdentifiers(t *testing.T) {
+	for _, test := range collectIdentifiersTests {
+		got := collectIdentifiers(test.args)
+		if !reflect.DeepEqual(got, test.ids) {
+			t.Errorf("collectIdentifiers failed for %+v", test.args)
+		}
+	}
+}
