1393e25d9d4f54fb11da7ac0b2f6b338fec517d1
itchyny <itchyny@hatena.ne.jp>
2017-06-11 12:41:43 +0900

    add tests for empty

--- identifier_test.go
+++ identifier_test.go
@@ -105,11 +105,56 @@ func Test_insert(t *testing.T) {
 	}
 }
 
+func Test_empty(t *testing.T) {
+	tests := []struct {
+		values   map[string]map[string]string
+		expected bool
+	}{
+		{
+			values:   nil,
+			expected: true,
+		},
+		{
+			values: map[string]map[string]string{
+				"foo": map[string]string{},
+			},
+			expected: true,
+		},
+		{
+			values: map[string]map[string]string{
+				"foo": map[string]string{
+					"bar": "",
+					"baz": "",
+				},
+			},
+			expected: true,
+		},
+		{
+			values: map[string]map[string]string{
+				"foo": map[string]string{
+					"bar": "",
+					"baz": "",
+				},
+				"bar": map[string]string{
+					"qux": "quux",
+				},
+			},
+			expected: false,
+		},
+	}
+	for _, test := range tests {
+		got := empty(test.values)
+		if got != test.expected {
+			t.Errorf("empty not correctly for %+v (expected: %+v, got: %+v)", test.values, test.expected, got)
+		}
+	}
+}
+
 func Test_lookup(t *testing.T) {
 	for _, test := range identifierTests {
 		got := lookup(test.values, &test.id)
 		if got != test.value {
-			t.Errorf("lookup not correct for %+v (found: %+v, got: %+v)", test.id, test.value, got)
+			t.Errorf("lookup not correct for %+v (expected: %+v, got: %+v)", test.id, test.value, got)
 		}
 	}
 }
